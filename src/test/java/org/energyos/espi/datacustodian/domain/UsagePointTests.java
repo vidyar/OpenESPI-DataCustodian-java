ab5f305494f58872afe03090a0cccc4aa073c5e9
Abhinav Shrestha and Andy Pliszka <pair+ashrestha+andy@pivotallabs.com>
2013-09-20 16:05:10 -0400

    Adds associateByUUID and createOrReplaceByUUID to UsagePointRepository

--- src/test/java/org/energyos/espi/datacustodian/domain/UsagePointTests.java
+++ src/test/java/org/energyos/espi/datacustodian/domain/UsagePointTests.java
@@ -18,12 +18,42 @@ package org.energyos.espi.datacustodian.domain;
 
 import org.junit.Test;
 
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
 import javax.validation.constraints.NotNull;
 import javax.xml.bind.annotation.XmlTransient;
+import java.util.Set;
 
+import static junit.framework.Assert.assertTrue;
+import static junit.framework.TestCase.assertFalse;
 import static org.energyos.espi.datacustodian.support.TestUtils.assertAnnotationPresent;
 
 public class UsagePointTests {
+
+    @Test
+    public void isValid() throws Exception {
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+        UsagePoint usagePoint = new UsagePoint();
+        usagePoint.setMRID("urn:uuid:E8E75691-7F9D-49F3-8BE2-3A74EBF6BFC0");
+        usagePoint.setServiceCategory(new ServiceCategory(ServiceCategory.ELECTRICITY_SERVICE));
+
+        Set<ConstraintViolation<UsagePoint>> violations = validator.validate(usagePoint);
+
+        assertTrue(violations.isEmpty());
+    }
+
+    @Test
+    public void isInvalid() throws Exception {
+        Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
+
+        UsagePoint usagePoint = new UsagePoint();
+
+        Set<ConstraintViolation<UsagePoint>> violations = validator.validate(usagePoint);
+
+        assertFalse(violations.isEmpty());
+    }
 
     @Test
     public void validations() {
